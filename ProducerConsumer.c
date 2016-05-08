0cd25ef72072f21e17e6cf44990cc12fb3da8985
Dandy Martin <dandy@siu.edu>
2016-05-08 18:19:36 -0500

    fix of fix of deadlock fix
    original "fix" was incorrect, semi-reversion of correct deadlock prevention

--- ProducerConsumer.c
+++ ProducerConsumer.c
@@ -18,8 +18,8 @@ int need = 0;
 int producers, consumers;
 
 // prototypes:
-int producer();
-int consumer();
+void producer();
+void consumer();
 
 
 int main(int argc, char *argv[]){
@@ -30,37 +30,43 @@ int main(int argc, char *argv[]){
         while((arg = getopt(argc, argv, "p:c:")) != -1){
             switch(arg){
                 case 'p':
-                    p_count = atoi(optarg);
+                    p_count = atoi(optarg); // assign number of producers
                 break;
                 case 'c':
-                    c_count = atoi(optarg);
+                    c_count = atoi(optarg); // assign number of consumers
                 break;
-                case '?':
+                case '?':   // incorrect arguments
                     abort();
                 default:
                 break;
             }
         }
     }
+
+    // assign values to counters
     producers = p_count;
     consumers = c_count;
     pthread_mutex_lock(&c_mutex);
     printf("Producer(s): %d\n", p_count);
     printf("Consumer(s): %d\n", c_count);
-    pthread_t *ptID = (pthread_t *) malloc(sizeof(pthread_t) * p_count);
-    pthread_t *ctID = (pthread_t *) malloc(sizeof(pthread_t) * c_count);
-   for(int p = 0; p < p_count; p++){
+    pthread_t *ptID = (pthread_t *) malloc(sizeof(pthread_t) * p_count); // producer thread ID memory allocation
+    pthread_t *ctID = (pthread_t *) malloc(sizeof(pthread_t) * c_count); // consumer thread ID memory allocation
+
+    // initialize producer threads
+    for(int p = 0; p < p_count; p++){
         if(pthread_create(ptID+p, NULL, (void *) &producer, NULL) != 0){
             perror("producer thread");
         }
     }
 
+    // initialize consumer threads
     for(int c = 0; c < c_count; c++){
         if(pthread_create(ctID+c, NULL, (void *) &consumer, NULL) != 0){
             perror("consumer thread");
         }
     }
 
+    // clean up clean up
     for(int i = 0; i < p_count; i++){
         pthread_join(ptID[i], NULL);
     }
@@ -68,38 +74,54 @@ int main(int argc, char *argv[]){
         pthread_join(ctID[j], NULL);
     }
 
+    // everybody everywhere
     pthread_mutex_destroy(&mutex);
     pthread_mutex_destroy(&mutex_2);
     pthread_mutex_destroy(&c_mutex);
+
+    // clean up clean up everybody do your share
+    free(ptID);
+    free(ctID);
     return 0;
 }
 
-int producer(){
+void producer(){
     int produce = 1 + ((double)rand() / (double) RAND_MAX) * 20;
+    printf("producer: mutex lock\n");
     pthread_mutex_lock(&mutex);
         producers--;
         product += produce;
         printf("Producer has stocked %d product.\n", produce);
         if(need > 0 && product > need){
             pthread_mutex_unlock(&c_mutex);
+            printf("producer: c_mutex unlock\n");
         }else{
             pthread_mutex_unlock(&mutex);
+            printf("producer: mutex unlock\n");
         }
-    return 0;
 }
 
-int consumer(){
+void consumer(){
     int take = ((double)rand() / (double) RAND_MAX) * 20;
+    printf("consumer: mutex_2 lock\n");
     pthread_mutex_lock(&mutex_2);
         printf("DEBUG CONSUMER COUNT: %d\n", consumers);
         printf("DEBUG PRODUCER COUNT: %d\n", producers);
         printf("TOTAL PRODUCT: %d\n", product);
-            if(producers == 0){// attempt to eliminate deadlocking issue
+
+            // deadlock protection
+            if(producers <= 0){
+
                 // wait and adjust consumption until it falls within tolerances
-                producers--;
                 while((take = ((double)rand() / (double) RAND_MAX) * 20) > product);
-                pthread_mutex_unlock(&mutex);
+
+                if(producers == 0){// first incidence of producer absence
+                    printf("deadlock protection: mutex unlock\n");
+                    pthread_mutex_unlock(&mutex);
+                    producers--;
+                }
             }
+        printf("consumer: mutex lock\n");
         pthread_mutex_lock(&mutex);
             if(product >= take){
                 product -= take;
@@ -107,9 +129,11 @@ int consumer(){
                 consumers--;
                 pthread_mutex_unlock(&mutex);
                 pthread_mutex_unlock(&mutex_2);
+                printf("consumer: mutex unlock\n");
             }else{
                 need = take;
                 pthread_mutex_unlock(&mutex);
+                printf("need: c_mutex lock\n");
                 pthread_mutex_lock(&c_mutex);
                     product -= take;
                     need = 0;
@@ -117,7 +141,7 @@ int consumer(){
                     consumers--;
                 pthread_mutex_unlock(&mutex);
                 pthread_mutex_unlock(&mutex_2);
+                printf("need: mutex unlock\n");
             }
-    return 0;
 }
 
